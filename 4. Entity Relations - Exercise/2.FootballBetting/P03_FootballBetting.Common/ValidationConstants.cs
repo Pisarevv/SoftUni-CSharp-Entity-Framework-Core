5e05c9c988855c8c376b2365342e22a6a0967bd8
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-28 15:21:16 +0200

    Color class added
    Added main skeleton of the class

--- 4. Entity Relations - Exercise/2.FootballBetting/P03_FootballBetting.Common/ValidationConstants.cs
+++ 4. Entity Relations - Exercise/2.FootballBetting/P03_FootballBetting.Common/ValidationConstants.cs
@@ -5,4 +5,8 @@ public class ValidationConstants
     //Team
     public const int MaxTeamNameLength = 100;
     public const int MaxLogoUrlLength = 2056;
+    public const int MaxInitialsLength = 5;
+
+    //Color
+    public const int MaxColorNameLength = 10;
 }
---
+++ 4. Entity Relations - Exercise/2.FootballBetting/P03_FootballBetting.Data.Models/Color.cs
@@ -0,0 +1,14 @@
+﻿using P03_FootballBetting.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P03_FootballBetting.Data.Models;
+
+
+public class Color
+{
+    [Key]
+    public int ColorId { get; set; }
+
+    [MaxLength(ValidationConstants.MaxColorNameLength)]
+    public string Name { get; set; } = null!;
+}
