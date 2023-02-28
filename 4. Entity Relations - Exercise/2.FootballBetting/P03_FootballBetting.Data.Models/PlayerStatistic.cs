98e251daaa062d0de53a40d6fb5cd9cac295bc72
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-28 15:45:24 +0200

    Added PlayerStatistic class

---
+++ 4. Entity Relations - Exercise/2.FootballBetting/P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,15 @@
+﻿namespace P03_FootballBetting.Data.Models;
+
+public class PlayerStatistic
+{
+    public int GameId { get; set; }
+
+    public int PlayerId { get; set; }
+    //TODO: Add relations
+
+    public byte ScoredGoals { get; set; }
+
+    public byte Assists { get; set; }
+
+    public byte MinutesPlayed { get; set; }
+}
