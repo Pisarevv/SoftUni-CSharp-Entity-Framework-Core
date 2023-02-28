36f0bc5b16ebab4e2d5aa092ee148a106718ca64
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-28 10:57:09 +0200

    Created course class

---
+++ 4. Entity Relations - Exercise/1.Student System/P01_StudentSystem.Data.Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using P01_StudentSystem.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models;
+
+public class Course
+{
+    [Key]
+    public int CourseId { get; set; }
+
+    [MaxLength(ValidationConstants.CourseNameMaxLenght)]
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+
+    public decimal Price { get; set; }
+
+
+}
