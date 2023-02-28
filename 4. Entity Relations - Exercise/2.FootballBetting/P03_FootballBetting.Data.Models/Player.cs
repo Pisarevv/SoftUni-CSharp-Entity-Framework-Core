165a0d050d40bb0a796a84a7083271df50eb63ab
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-28 15:35:46 +0200

    Added player class

---
+++ 4. Entity Relations - Exercise/2.FootballBetting/P03_FootballBetting.Data.Models/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace P03_FootballBetting.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using Common;
+
+public class Player
+{
+    [Key]
+    public int PlayerId { get; set; }
+
+    [MaxLength(ValidationConstants.MaxPlayerNameLength)]
+    public string Name { get; set; } = null!;
+
+    [MaxLength(ValidationConstants.MaxSquadNameLength)]
+    public string SquadName { get; set; } = null!;
+
+    public int TeamId { get; set; }
+
+    public int PositionId { get; set; }
+
+    public bool IsInjured { get; set; }
+}
