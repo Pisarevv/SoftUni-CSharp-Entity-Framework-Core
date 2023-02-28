ef58ec5fc72bcc79d78bfbc844581ac637f37b08
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-02-28 11:20:02 +0200

    Homework class created

--- 4. Entity Relations - Exercise/1.Student System/P01_StudentSystem.Data.Models/Enums/ResourceType.cs
+++ 4. Entity Relations - Exercise/1.Student System/P01_StudentSystem.Data.Models/Enums/ResourceType.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace P01_StudentSystem.Data.Models.Enums;
 
-namespace P01_StudentSystem.Data.Models.Enums;
-internal class ResourceType
+public enum ResourceType
 {
+    Video = 0,
+    Presentation = 1,
+    Document = 2,
+    Other = 3
 }
---
+++ 4. Entity Relations - Exercise/1.Student System/P01_StudentSystem.Data.Models/Homework.cs
@@ -0,0 +1,22 @@
+﻿using P01_StudentSystem.Common;
+using P01_StudentSystem.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_StudentSystem.Data.Models;
+
+public class Homework
+{
+    [Key]
+    public int HomeworkId { get; set; }
+
+    [MaxLength(ValidationConstants.HomeworkContentMaxLenghth)]
+    public string Content { get; set; } = null!;
+
+    public ContentType ContentType { get; set; }
+
+    public DateTime SubmissionTime { get; set; }
+
+    public int StudentId { get; set; }
+
+    public int CourseId { get; set; }
+}
