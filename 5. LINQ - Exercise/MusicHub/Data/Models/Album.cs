5d0322cb32fdbdcfa9df9a596685015ac0a743c5
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-02 16:06:30 +0200

    Added Album class

---
+++ 5. LINQ - Exercise/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,28 @@
+﻿namespace MusicHub.Data.Models;
+
+using Common;
+using System.ComponentModel.DataAnnotations;
+
+public class Album
+{
+    public Album()
+    {
+       this.Songs = new HashSet<Song>(); 
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxAlbumNameLength)]
+    public string Name { get; set; } = null!;
+
+    public DateTime ReleaseDate { get; set; }
+
+    public decimal Price => this.Songs.Sum(s => s.Price);
+
+    public int ProducerId { get; set; }
+
+    //Todo: add Producer
+
+    public ICollection<Song> Songs { get; set; }
+}
