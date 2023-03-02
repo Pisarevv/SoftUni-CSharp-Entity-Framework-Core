c7308cd21ed02f05ecfb095377de4d4b4b3d1375
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-02 16:16:10 +0200

    Producer class added

---
+++ 5. LINQ - Exercise/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,24 @@
+﻿namespace MusicHub.Data.Models;
+
+using Common;
+using System.ComponentModel.DataAnnotations;
+
+public class Producer
+{
+    public Producer()
+    {
+       this.Albums = new HashSet<Album>(); 
+    }
+
+    [Key] 
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxProducerNameLength)]
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    ICollection<Album> Albums { get; set; }
+}
