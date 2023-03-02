522f65938e9035683d53ef309c035f580102411a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-02 16:12:20 +0200

    Added Performer class

---
+++ 5. LINQ - Exercise/MusicHub/Data/Models/Performer.cs
@@ -0,0 +1,22 @@
+﻿namespace MusicHub.Data.Models;
+
+using Common;
+using System.ComponentModel.DataAnnotations;
+
+public class Performer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxPerformerFirstNameLength)]
+    public string FirstName { get; set; } = null!;
+
+    [MaxLength(ValidationConstants.MaxPerformerLastNameLength)]
+    public string LastName { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public decimal NetWorth { get; set; }
+
+    //Todo: add perforersongs collection
+}
