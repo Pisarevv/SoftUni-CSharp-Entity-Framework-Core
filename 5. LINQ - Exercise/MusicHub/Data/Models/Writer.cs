2f88f1b260020f1c2298243f6dad72c30cec1e7a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-02 16:19:08 +0200

    Added Writer class

---
+++ 5. LINQ - Exercise/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,22 @@
+﻿namespace MusicHub.Data.Models;
+
+using Common;
+using System.ComponentModel.DataAnnotations;
+
+public class Writer
+{
+    public Writer()
+    {
+        this.Songs = new HashSet<Song>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.MaxWriterNameLength)]
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym { get; set; }
+
+    public ICollection<Song> Songs { get; set; }
+}
