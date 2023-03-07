968a0048fbcff6c213e5887a6b5fef1226410d2b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-07 14:33:05 +0200

    Added Book and Category classes

--- 6. Advanced Querying - Exercise/BookShopSystem/BookShop.Common/ValidationConstants.cs
+++ 6. Advanced Querying - Exercise/BookShopSystem/BookShop.Common/ValidationConstants.cs
@@ -2,4 +2,12 @@
 
 public class ValidationConstants
 {
+    //Author
+    public const int AuthorFirstNameMaxLength = 50;
+    public const int AuthorLastNameMaxLength = 50;
+
+    //Book
+    public const int BookTitleMaxLength = 50;
+    public const int BookDescriptionMaxLength = 1000;
 }
+
---
+++ 6. Advanced Querying - Exercise/BookShopSystem/BookShop.Models/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop.Models;
+
+using BookShop.Models.Enums;
+using Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Book
+{
+    [Key]
+    public int BookId { get; set; }
+
+    [MaxLength(ValidationConstants.BookTitleMaxLength)]
+    public string Title { get; set; } = null!;
+
+    [MaxLength(ValidationConstants.BookDescriptionMaxLength)]
+    public string Description { get; set; } = null!;
+
+    public DateTime? ReleaseDate { get; set; }
+
+    public int Copies { get; set; }
+
+    public decimal Price { get; set; }
+
+    public EditionType EditionType { get; set; }
+
+    public AgeRestriction AgeRestriction { get; set; }
+
+    [ForeignKey(nameof(Author))]
+    public int AuthorId { get; set; }
+
+    public Author Author { get; set; } = null!;
+ 
+    //TODO : Add mapping table 
+}
