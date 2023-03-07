f9f0fb74cb631dfffee575f397eba86ae8cb32b6
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-07 15:22:24 +0200

    Update

--- 6. Advanced Querying - Exercise/BookShopSystem/BookShop.Common/DbConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BookShop.Common;
-
-public class DbConfig
-{
-    public const string ConnectionString = @"Server=.\SQLEXPRESS;Database=MusicHub;Integrated Security=true;TrustServerCertificate = true";
-}
