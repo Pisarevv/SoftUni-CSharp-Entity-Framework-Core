91175d74e8ee6e94546ddf82b681d45924063be0
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-18 20:45:40 +0200

    Added XmlHelper class with methods for XML deserialization

--- 9. XML Processing - Exercise/09.XML-Processing-Exercises-ProductShop-6.0/ProductShop/StartUp.cs
+++ 9. XML Processing - Exercise/09.XML-Processing-Exercises-ProductShop-6.0/ProductShop/StartUp.cs
@@ -1,10 +1,27 @@
-﻿namespace ProductShop
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Utilities;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            ProductShopContext context = new ProductShopContext();
 
+            var inputXML = File.ReadAllText("../../../Datasets/users.xml");
+           
+            var result = ImportProducts(context, inputXML);
+            Console.WriteLine(result);
+        }
+
+        //Problem 1
+        public static string ImportProducts(ProductShopContext context, string inputXml)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ 9. XML Processing - Exercise/09.XML-Processing-Exercises-ProductShop-6.0/ProductShop/Utilities/XmlHelper.cs
@@ -0,0 +1,45 @@
+﻿using ProductShop.DTOs.Import;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ProductShop.Utilities;
+
+public class XmlHelper
+{
+    public T Deserialize<T>(string inputXml,string rootName)
+    {
+
+        using StringReader reader = new StringReader(inputXml);
+
+        string xmlRootName = GetRootName(inputXml);
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(xmlRootName);
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+        T result = (T)xmlSerializer.Deserialize(reader);
+
+        return result;
+             
+    }
+
+    public T[] DeserializeCollection<T>(string inputXml, string rootName)
+    {
+
+        using StringReader reader = new StringReader(inputXml);
+
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+        T[] result = (T[])xmlSerializer.Deserialize(reader);
+
+        return result;
+
+    }
+
+    public string GetRootName(string inputXml)
+    {
+        XmlDocument xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(inputXml);
+        return xmlDocument.DocumentElement.Name.ToString();
+    }
+}
