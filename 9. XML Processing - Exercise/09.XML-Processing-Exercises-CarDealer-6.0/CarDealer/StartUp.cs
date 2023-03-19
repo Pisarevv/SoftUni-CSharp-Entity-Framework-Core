7634f3452210e304561e9903859d814ff9362e88
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-19 13:33:30 +0200

    Project classes refracturing and added XmlHelper class

--- 9. XML Processing - Exercise/09.XML-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
+++ 9. XML Processing - Exercise/09.XML-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -1,10 +1,21 @@
-﻿namespace CarDealer
+﻿using AutoMapper;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+             
+        }
 
+
+        public static IMapper createMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
         }
     }
 }
---
+++ 9. XML Processing - Exercise/09.XML-Processing-Exercises-CarDealer-6.0/CarDealer/Utilities/IXmlHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductShop.Utilities;
+public interface IXmlHelper
+{
+    public T Deserialize<T>(string inputXml);
+
+    public T[] DeserializeCollection<T>(string inputXml);
+
+    public string Serialize<T>(T[] input,string rootName);
+
+    public string Serialize<T>(T input, string rootName);
+}
