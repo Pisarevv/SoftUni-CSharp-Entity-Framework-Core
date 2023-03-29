ccac668fa55e462a5ef8dc25e49198db74752e78
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-03-29 22:23:05 +0300

    Task 3 of exporting data completed

--- 14.Exam preparation/Databases Advanced Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
+++ 14.Exam preparation/Databases Advanced Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
@@ -1,18 +1,67 @@
 ﻿namespace Trucks.DataProcessor
 {
     using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using ProductShop.Utilities;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
         {
-            throw new NotImplementedException();
+            IMapper mapper = CreateMapper();
+            IXmlHelper xmlHelper = new XmlHelper();
+
+            var despatchers = context.Despatchers
+                              .Where(d => d.Trucks.Count > 0)
+                              .OrderByDescending(d => d.Trucks.Count)
+                              .ThenBy(d => d.Name)
+                              .ProjectTo<ExportDespatcherDto>(mapper.ConfigurationProvider)
+                              .AsNoTracking()
+                              .ToArray();
+
+            var result = xmlHelper.Serialize<ExportDespatcherDto[]>(despatchers, "Despatchers");
+
+            return result.ToString();
         }
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
         {
-            throw new NotImplementedException();
+            var clients = context.Clients
+                          .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                          .ToArray()
+                          .Select(c => new
+                          {
+                              Name = c.Name,
+                              Trucks = c.ClientsTrucks
+                                         .Select(ct => ct.Truck)
+                                         .Where(t => t.TankCapacity >= capacity)
+                                         .Select(t => new
+                                         {
+                                             TruckRegistrationNumber = t.RegistrationNumber,
+                                             VinNumber = t.VinNumber,
+                                             TankCapacity = t.TankCapacity,
+                                             CargoCapacity = t.CargoCapacity,
+                                             CategoryType = t.CategoryType.ToString(),
+                                             MakeType = t.MakeType.ToString()
+                                         })
+                                         .OrderBy(x => x.MakeType)
+                                         .ThenByDescending(x => x.CargoCapacity)
+                                         .ToArray()
+                          })
+                          .OrderByDescending(x => x.Trucks.Count())
+                          .ThenBy(x => x.Name)
+                          .Take(10)
+                          .ToArray();
+
+            var result = JsonConvert.SerializeObject(clients, Formatting.Indented);
+
+            return result;
+                          
         }
 
         private static IMapper CreateMapper()
--- 14.Exam preparation/Databases Advanced Exam - 15 August 2022/Trucks/TrucksProfile.cs
+++ 14.Exam preparation/Databases Advanced Exam - 15 August 2022/Trucks/TrucksProfile.cs
@@ -2,6 +2,7 @@
 {
     using AutoMapper;
     using Trucks.Data.Models;
+    using Trucks.DataProcessor.ExportDto;
     using Trucks.DataProcessor.ImportDto;
 
     public class TrucksProfile : Profile
@@ -14,9 +15,18 @@
                 //Truck
                 this.CreateMap<ImportTruckDto, Truck>();
 
+                this.CreateMap<Truck, ExportTruckDto>()
+                .ForMember(d => d.Make, obj => obj.MapFrom(s => s.MakeType.ToString()));
+               
+
                 //Dispatcher
                 this.CreateMap<ImportDespatcherDto, Despatcher>()
                 .ForMember(d => d.Trucks, obj => obj.Ignore());
+
+                this.CreateMap<Despatcher, ExportDespatcherDto>()
+                .ForMember(d => d.TrucksCount, obj => obj.MapFrom(s => s.Trucks.Count))
+                .ForMember(d => d.DespatcherName, obj => obj.MapFrom(s => s.Name))
+                .ForMember(d => d.Trucks, obj => obj.MapFrom(s => s.Trucks.ToArray().OrderBy(t => t.RegistrationNumber)));
 
                 //Client
                 this.CreateMap<ImportClientDto, Client>()
