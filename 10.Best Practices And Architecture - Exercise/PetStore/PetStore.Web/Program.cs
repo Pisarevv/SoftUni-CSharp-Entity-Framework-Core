12944a4bc20a2494fad3756ce62b560daa3b59d7
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-10 20:39:34 +0300

    Update
    Added automapper

--- 10.Best Practices And Architecture - Exercise/PetStore/PetStore.Web/Program.cs
+++ 10.Best Practices And Architecture - Exercise/PetStore/PetStore.Web/Program.cs
@@ -8,19 +8,7 @@ namespace PetStore.Web
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
+            WebApplication app = ConfigurateServices(args);
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -50,6 +38,41 @@ namespace PetStore.Web
             app.Run();
         }
 
+        private static WebApplication ConfigurateServices(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connectionString = builder
+                .Configuration
+                .GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            builder
+                .Services
+                .AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder
+                .Services
+                .AddDatabaseDeveloperPageExceptionFilter();
+
+            builder
+                .Services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder
+                .Services
+                .AddControllersWithViews();
+
+            builder
+                .Services
+                .AddAutoMapper(typeof(Program));
+
+            WebApplication app = builder.Build();
+
+            return app;
+        }
 
     }
 }
