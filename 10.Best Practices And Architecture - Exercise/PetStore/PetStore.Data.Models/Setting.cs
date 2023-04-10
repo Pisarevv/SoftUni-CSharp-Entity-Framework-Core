b05b38164f6fa53173ee3746a28abc79cb3a31bc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-04-10 20:11:07 +0300

    Project initialzization
    Minor changes are rearrangement of files

---
+++ 10.Best Practices And Architecture - Exercise/PetStore/PetStore.Data.Models/Setting.cs
@@ -0,0 +1,14 @@
+﻿namespace PetStore.Data.Models
+{
+    using PetStore.Data.Common.Models;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Value { get; set; } = null!;
+    }
+}
